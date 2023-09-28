a2744e89ae52cf4ad23af7f2fbcf2c07f410fb7e
Rohan <rohandute1@gmail.com>
2023-09-28 15:59:21 +0530

    [rohan]successfully performed duplicate counter program

---
+++ Day4B_Object_Oriented_Practice_Problem/DuplicateCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_Object_Oriented_Practice_Problem
+{
+    class DuplicateCounter
+    {
+        private int[] arr;
+
+        public DuplicateCounter(int[] arr)
+        {
+            this.arr = arr;
+        }
+
+        public int CountDuplicates()
+        {
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- Day4B_Object_Oriented_Practice_Problem/Program.cs
+++ Day4B_Object_Oriented_Practice_Problem/Program.cs
@@ -11,7 +11,39 @@ namespace Day4B_Object_Oriented_Practice_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 4B OOP's Practice Problems.");
+            bool continueExecution = true;
+            while (continueExecution)
+            {
+                Console.WriteLine("Please choose program which you want to perform.");
+                Console.WriteLine("1.Duplicate Counter\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Console.Write("Enter the number of elements in the array: ");
+                        int n = int.Parse(Console.ReadLine());
 
+                        int[] arr = new int[n];
+                        Console.WriteLine("Enter the elements of the array:");
+
+                        for (int i = 0; i < n; i++)
+                        {
+                            arr[i] = int.Parse(Console.ReadLine());
+                        }
+
+                        DuplicateCounter counter = new DuplicateCounter(arr);
+                        int duplicateCount = counter.CountDuplicates();
+
+                        Console.WriteLine($"Total number of duplicate elements: {duplicateCount}");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue (yes or no)");
+                string userInput = Console.ReadLine().ToLower();
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                }
+            }
             Console.ReadLine();
         }
     }
